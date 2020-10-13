7b3885878a57f60a680c40f2cfcec354fc09081b
MerinAlex23 <53620303+MerinAlex23@users.noreply.github.com>
2020-10-14 02:42:09 +0530

    Add files via upload

---
+++ DoubleEndedQueue.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define N5;
+
+int front=-1,rear=-1;
+int deque[N];
+void enqueuefront(int x){
+if()(f==0&&R==N-1)||(front==rear+1))
+{
+  printf("queue is full");
+
+}
+else if(front==-1&&rear==-1){
+  front=rear=0;
+  deque[Front]=x;
+
+}
+else if(front==0){
+  front=N-1;
+deque[front]=x
+}
+else{
+  front--;
+deque[front]=x;
+}
+}
+void enqueuerear(int x){
+  if((front==0&&R=N-1)||(front==rear)){
+    printf("queue is full")
+  }
+  else if(front==-1&&rear==-1){
+    front=rear=0;
+    deque[rear]=x;
+
+  }
+  else if{
+    rear=0;
+    deque[rear]=x;
+
+  }
+  else{
+    
+  }
+}
